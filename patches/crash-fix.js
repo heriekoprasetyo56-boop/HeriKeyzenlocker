68577acdec26340b1de975e3034bc8e8c852b31d
HeriTezzRorw <heriekoprasetyo56@gmail.com>
2026-04-01 17:26:44 +0700

    Delete patches directory

--- patches/crash-fix.js
+++
@@ -1,9 +0,0 @@
-async function handleConnectionCrash(update) {
-  const { connection } = update;
-  if (connection === 'close') {
-    await new Promise(r => setTimeout(r, 5000));
-    return true;
-  }
-  return false;
-}
-module.exports = { handleConnectionCrash };
